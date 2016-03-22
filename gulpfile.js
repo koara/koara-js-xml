9e1086d0786cb3ce51719e096367a19fbb36c153
Andy Van Den Heuvel <andy.vandenheuvel@gmail.com>
2016-03-22 07:10:14 +0100

    temporary test without coverage

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+'use strict';
+
+var gulp = require('gulp');
+var browserify = require('browserify');
+var buffer = require('vinyl-buffer');
+var cover = require('gulp-coverage');
+var coveralls = require('gulp-coveralls');
+var jasmine = require('gulp-jasmine');
+var eslint = require('gulp-eslint');
+var uglify = require('gulp-uglify');
+var reactify = require('reactify');
+var rename = require('gulp-rename');
+var source = require('vinyl-source-stream');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('default', ['lint', 'bundle', 'test']);
+
+gulp.task('bundle', function() {
+	var b = browserify({ entries: './index.js', standalone: 'koaraXml', debug: true, transform: [reactify] });
+	return b.bundle()
+	    .pipe(source('index.js'))
+	    .pipe(rename('koara-xml.js'))
+	    .pipe(buffer())
+	    .pipe(gulp.dest('dist'))
+	    .pipe(rename('koara-xml.min.js'))
+	    .pipe(sourcemaps.init({loadMaps: true}))
+	    .pipe(uglify())
+	    .pipe(sourcemaps.write('.'))
+	    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('lint', function() {
+  return gulp.src(['lib/koara/**/*.js'])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+});
+
+gulp.task('test', function () {
+	return gulp.src('test/*.js').pipe(jasmine());
+});
+
+gulp.task('test-travisci', function () {
+	return gulp.src('test/*.js')
+      //.pipe(cover.instrument({ pattern: ['lib/*.js'] }))
+      .pipe(jasmine());
+      //.pipe(cover.gather())
+      //.pipe(cover.format({ reporter: 'lcov' }))
+      //.pipe(coveralls());
+});
